820bafc802a24ededc57e446453b60fc0f0e1290
MITCHYUGAN <denmichigan123@gmail.com>
2022-11-03 14:56:49 +0100

    Braig tools landing page

---
+++ js/about.js
@@ -0,0 +1,26 @@
+const nav = document.querySelector("nav"),
+            toggleBtn = nav.querySelector('.togglebutton')
+
+toggleBtn.addEventListener("click", () =>{
+    nav.classList.toggle("open");
+});           
+
+function ondrag({movementY}){
+    const navStyle = window.getComputedStyle(nav);
+    navTop = parseInt(navStyle.top),
+    navHeight = parseInt(navStyle.height),
+    windHeight= window.innerHeight;
+    
+    nav.style.top = navTop > 0 ? `${navTop + movementY}px` : "1px";
+
+    if( navTop > windHeight - navHeight){
+    nav.style.top = `${windHeight - navHeight}px`
+    }
+}
+
+nav.addEventListener('mousedown', () =>{
+    nav.addEventListener('mousemove', ondrag);
+});
+nav.addEventListener('mouseup', () =>{
+    nav.removeEventListener('mousemove', ondrag);
+});
